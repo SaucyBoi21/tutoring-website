236dce54c8222656dc7cd1be1893d80d4a08ee9d
Saahas Swaroop <saahas.swaroop@googlemail.com>
2025-01-13 15:47:34 -0500

    rate component

--- app/src/app/pages/rates/page.tsx
+++ app/src/app/pages/rates/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Rate from "./rate";
 
 export default function Rates() {
-    return (
-        <>
-            <h1>Rates</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Rates</h1>
+      <Rate className="K-8 Math" classRate="$45 per hour" />
+      <Rate className="High School Math" classRate="$55 per hour" />
+      <Rate className="SAT" classRate="$65 per hour" />
+    </>
+  );
 }
